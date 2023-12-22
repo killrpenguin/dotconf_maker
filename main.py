13eb8a18aeb7eea9280976ebd6eb11f8f19e323b
David <dmcfarland8031@gmail.com>
2023-12-21 19:59:15 -0700

    added user settings from system property

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from os import system
+import subprocess
 import re
 
 
@@ -8,7 +8,7 @@ class Dot_Conf:
         pass
 
     @property  # {config name: mode or setting}
-    def user_settings(self):
+    def user_settings_from_file(self: dict) -> dict:
         user_settings_hmap = {}
         with open("user_settings") as dot_config:
             dot_config = dot_config.read().strip().split()
@@ -18,13 +18,25 @@ class Dot_Conf:
                 if val is not None:
                     txt = val.group().split("=", 1)
                     user_settings_hmap.update({txt[0]: txt[1]})
-                    # self.user_settings_user_settings_hmap.update({txt[0]:txt[1]})
         return user_settings_hmap
 
-    def get_config(self):
-        pass
+    @property
+    def user_settings_from_system(self: dict) -> dict:
+        user_settings_sys = {}
+        lspci_drivers = subprocess.run(["lspci", "-nnk"], stdout=subprocess.PIPE)
+        lspci_drivers = lspci_drivers.stdout.decode("utf-8").split("\n")
+        lspci_drivers = [re.search("[es]:.*", txt) for txt in lspci_drivers]
+        for driver in lspci_drivers:
+            if driver is not None:
+                print(f"{driver}")
+                txt = driver.group().strip().split(" ")
+                user_settings_sys.update({txt[1].upper():"y"})
+        return user_settings_sys
 
+    def compare(self, *args, **kwargs) -> any:
+        for itm in self.user_settings_from_system.items():
+            print(itm[0])
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     testing = Dot_Conf()
-    print(testing.user_settings)
+    testing.compare()
