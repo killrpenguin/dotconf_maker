f81fc3fa315b9c84e5b86d339f15e2cc30bdcf15
David <dmcfarland8031@gmail.com>
2023-12-21 18:45:19 -0700

    User settings turned into .config class property.

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from os import system
 import re
 
-# grep -e 'CONFIG_*=y' /usr/src/linux/.config | grep -v "not set" /usr/src/linux/.config > ~/Documents/config_file
-class My_Dot_Conf:
-    def __init__(self):
-        self.user_settings_hmap = {} #hash map key: {config name: mode or setting}
 
+# grep -e 'CONFIG_*=y' /usr/src/linux/.config | grep -v "not set" /usr/src/linux/.config > ~/Documents/config_file
+class Dot_Conf:
+    def __init__(self):
+        pass
+
+    @property  # {config name: mode or setting}
     def user_settings(self):
+        user_settings_hmap = {}
         with open("user_settings") as dot_config:
             dot_config = dot_config.read().strip().split()
             # regex searches for strings starting with "CON"
@@ -14,14 +17,14 @@ class My_Dot_Conf:
             for val in dot_config:
                 if val is not None:
                     txt = val.group().split("=", 1)
-                    self.user_settings_hmap.update({txt[0]:txt[1]})
-
-            for tst in self.user_settings_hmap.items():
-                print(f"{tst}")
+                    user_settings_hmap.update({txt[0]: txt[1]})
+                    # self.user_settings_user_settings_hmap.update({txt[0]:txt[1]})
+        return user_settings_hmap
 
     def get_config(self):
         pass
 
+
 if __name__ == '__main__':
-    testing = My_Dot_Conf()
-    testing.user_settings()
+    testing = Dot_Conf()
+    print(testing.user_settings)
